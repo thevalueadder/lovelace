f1d73e08462fd37815de29280831533ee0ca4e03
thevalueadder <92184734+thevalueadder@users.noreply.github.com>
2021-10-16 14:35:55 +0100

    Create stage_0_EstherOpone.py

---
+++ stage_0_EstherOpone.py
@@ -0,0 +1,16 @@
+name = "Esther Opone"
+email = "oponeesther12@gmail.com"
+slack_username = "@thevalueadder"
+biostack = "Genomics"
+twitter_handle = "@thevalueadder_"
+
+def hammD(su,th):
+	hd = 0
+	for i in range(len(su)):
+		if su[i] != th[i]:
+			hd += 1
+	return hd
+
+hamming_distance = hammD(slack_username, twitter_handle)
+
+print(name,email,slack_username,biostack,twitter_handle,hamming_distance, sep="\n")
