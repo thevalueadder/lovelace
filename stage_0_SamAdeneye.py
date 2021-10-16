740e3d3aadc098d65f854a570b1c9e298c1b780b
thevalueadder <92184734+thevalueadder@users.noreply.github.com>
2021-10-16 15:12:51 +0100

    Create stage_0_SamAdeneye.py

---
+++ stage_0_SamAdeneye.py
@@ -0,0 +1,8 @@
+name = "ADENEYE, Samuel Adewale" 
+email = "samueladeneye75@gmail.com" 
+slack_username = "SamAdeneye"
+biostack = "Data Science and Machine Learning" 
+twitter_username = "@Unkool_Sam"
+hamming distance = 1
+print(name,email,slack_username,biostack,twitter_handle,hamming_distance
+
