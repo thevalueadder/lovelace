386ec863303aa3ee7b3f521eb0c52c01ef30eb69
JuanMa Garcia <juanmgar@gmail.com>
2021-10-16 18:56:18 +0200

    Update stage_0_SamAdeneye.py

--- stage_0_SamAdeneye.py
+++ stage_0_SamAdeneye.py
@@ -1,12 +1,16 @@
-name = "ADENEYE Samuel Adewale"
-email = "samueladeneye75@gmail.com"
-slack_username = "@SamAdeneye"
-biostack = "Data Science and Machine Learning"
-twitter_username = "Unkool_Sam"
+name = "Samuel Adeneye" 
+email = "samueladeneye75@gmail.com" 
+slack_username = "SamAdeneye"
+biostack = "Data Science and Machine Learning" 
+twitter_handle = "@Unkool_Sam"
 
+def hammD(su,th):
+	hd = 0
+	for i in range(len(su)):
+		if su[i] != th[i]:
+			hd += 1
+	return hd
 
-print (name)
-print (email)
-print (slack_username)
-print (biostack)
-print (twitter_username)
+hamming_distance = hammD(slack_username, twitter_handle)
+
+print name +', '+email+', '+slack_username+', '+biostack+', '+twitter_handle+',',hamming_distance
