76f03c23cbd27b6cd1db40da544080def2801df3
SalmaElShafie <salma4@aucegypt.edu>
2021-10-16 12:47:40 +0200

    Update csv_generator.sh

--- csv_generator.sh
+++ csv_generator.sh
@@ -9,5 +9,6 @@ echo "***Mixing personal info...***"
 echo "Name,email,slack_username,biostack,twitter_handle,hamming_distance">>team_lovelace.csv
 node stage_0_JuanmaGarcia.js >>team_lovelace.csv
 node stage_0_ToheebAmoo.js >>team_lovelace.csv
+node stage_0_SalmaElShafie.js >>team_lovelace.csv
 
 echo "***CSV generated!***"
